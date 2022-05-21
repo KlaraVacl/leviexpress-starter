518c9421c5d43d4b0d9fbc701bbca2a6ad0bbdd1
Klára Václavíková <klarka.vaclvikovaa@gmail.com>
2022-05-21 11:04:00 +0200

    Routování_done

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
 import { Home } from '../Home';
+import { Reservation } from '../Reservation';
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
 
 export const App = () => (
   <>
     <Header />
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="reservation" element={<Reservation />} />
+      </Routes>
+    </BrowserRouter>
     <Footer />
   </>
 );
